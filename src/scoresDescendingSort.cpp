dc9c3a9a727d543b42cf7e738b93dc4acc93882a
kavya9 <vkavya.13.cse@anits.edu.in>
2015-11-10 20:26:15 +0530

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -1,7 +1,9 @@
 /*
-OVERVIEW:  You are given scores for students of a class in alphabetical order. Write a function that sorts these scores in descending order.
+OVERVIEW:  You are given scores for students of a class in alphabetical order. Write a
+function that sorts these scores in descending order.
 Input is array of structures of type student (each student has name and score).
-E.g.: If input is [ { "stud1", 20 }, { "stud2", 30 }, { "stud3", 40 } ] (three students with marks 20, 30 , 40).
+E.g.: If input is [ { "stud1", 20 }, { "stud2", 30 }, { "stud3", 40 } ] 
+(three students with marks 20, 30 , 40).
 Output for this will be [ { "stud3", 40 }, { "stud2", 30 }, { "stud1", 20 } ]
 
 INPUTS: Array of structures of type student, length of array.
@@ -21,5 +23,19 @@ struct student {
 };
 
 void * scoresDescendingSort(struct student *students, int len) {
-	return NULL;
+	if (students == NULL || len < 0)return NULL;
+	int i,j;
+	struct student temp;
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < len - i - 1; j++)
+		{
+			if (students[j].score < students[j + 1].score)
+			{
+				temp = students[j];
+				students[j] = students[j + 1];
+				students[j + 1] = temp;
+			}
+		}
+	}
 }
