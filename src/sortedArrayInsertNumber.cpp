1499faf52b83748cab0331ad4defae047f7dd959
kavya9 <vkavya.13.cse@anits.edu.in>
2015-11-10 19:56:59 +0530

    Submit Commit

--- src/sortedArrayInsertNumber.cpp
+++ src/sortedArrayInsertNumber.cpp
@@ -1,6 +1,8 @@
 /*
-OVERVIEW: Given a sorted array, insert a given number into the array at appropriate position.
-			E.g.: Arr = [2, 4, 6], num = 5 final Arr = [2, 4, 5, 6]. 5 is inserted at 2nd index.
+OVERVIEW: Given a sorted array, insert a given number into the array at appropriate
+position.
+			E.g.: Arr = [2, 4, 6], num = 5 final Arr = [2, 4, 5, 6]. 5 is inserted at 2nd 
+			index.
 
 INPUTS: Integer	Array pointer, length of the array, number to be inserted.
 
@@ -16,5 +18,27 @@ NOTES: Use realloc to allocate memory.
 
 int * sortedArrayInsertNumber(int *Arr, int len, int num)
 {
-	return NULL;
+	if (Arr == NULL || len < 0)return NULL;
+	Arr = (int *)realloc(Arr, (len + 1)*sizeof(int));
+	int i,j,flag=0;
+	if (num>Arr[len - 1])
+	{
+		Arr[len] = num; return Arr;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (num <= Arr[i])
+		{
+			for (j = i + 1; j < len; j++)
+			{
+				Arr[j + 1] = Arr[j];
+			}
+			flag++;
+		}
+		if (flag == 1)
+		{
+			Arr[i] = num;
+			return Arr;
+		}
+	}
 }
